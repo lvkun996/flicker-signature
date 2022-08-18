70574c81c7f272dd3fff9510473f53ebc51136ff
lvkun996 <1549630250@qq.com>
2022-08-18 12:04:18 +0800

    fix:pc-mousemove

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -33,8 +33,8 @@ export function getHandlerKey () {
   } else {
     return {
       start: 'mousedown',
-      move: 'mouseover',
-      end: 'mouseleave'
+      move: 'mousemove',
+      end: 'mouseup'
     }
   }
 }
